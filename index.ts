c5f22c40ee8d0e61b9206dd01e29c731db1a7f05
Sergio Xalambrí <hello@sergiodxa.com>
2025-10-29 00:15:57 -0500

    feat: initialize project with Bun setup, including configuration files and basic API

---
+++ index.ts
@@ -0,0 +1,8 @@
+export default {
+  async fetch(request: Request) {
+    const url = new URL(request.url);
+    const name = url.searchParams.get("name") || "World";
+
+    return Response.json({ message: `Hello ${name}!` });
+  },
+};
